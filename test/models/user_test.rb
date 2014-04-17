eadadd7c0a8d8fb5be332422505d9bb0f69bce40
nkj20932 <nkj20932@hotmail.com>
2014-04-18 00:56:15 +0800

    writing tests
    validates first_name
    validates last_name
    validates profile_name and its uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,5 +4,29 @@ class UserTest < ActiveSupport::TestCase
   test "a user should enter a first name" do
   	user = User.new
   	assert !user.save
+  	assert !user.errors[:first_name].empty?
   end
+
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	user.profile_name = users(:adler).profile_name
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should enter a profile name with the right format" do
+  	user = User.new
+  	user.profile_name = "has blank"
+  	
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("profile should be in the right format.")
+  end
+
 end
